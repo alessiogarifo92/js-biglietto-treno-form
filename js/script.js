840f819322329e30619e1bdf08d0aadb39a3d70a
alessiogarifo92 <alessiogarifo92@gmail.com>
2020-10-21 17:16:58 +0200

    non appare costo biglietto

--- js/script.js
+++ js/script.js
@@ -6,24 +6,30 @@ var adulto = document.getElementById('adulto');
 var over = document.getElementById('over');
 var genera = document.getElementById('genera');
 var annulla = document.getElementById('annulla');
-var passeggero = document.getElementById('passegero');
+var passeggero = document.getElementById('passeggero');
 var offerta = document.getElementById('offerta');
 var carrozza = document.getElementById('carrozza');
 var codcp = document.getElementById('codecp');
 var costobiglietto = document.getElementById('costobiglietto');
 
 // biglietto adulto
-costobiglietto = parseInt(km * 0.21);
+costobiglietto = parseInt(km.value * 0.21);
 
 // biglietto sconto 20% minori 18
-if (minorenne === minore 18 anni){
+if ( minorenne === "minore 18 anni" ){
   costobiglietto = costobiglietto - ((costobiglietto / 100)* 20).toFixed(2);
-} else if (over === over 65 anni) {
+} else if (over === "over 65 anni") {
   // biglietto sconto 40% over 65
   costobiglietto = costobiglietto - ((costobiglietto / 100)* 40).toFixed(2);
 }
 
 genera.addEventListener('click',
  function() {
-   passegero.innerHTML = nome.value;
+   passeggero.innerHTML = nome.value;
+   costobiglietto.innerHTML = (if ( minorenne === "minore 18 anni" ){
+     costobiglietto = costobiglietto - ((costobiglietto / 100)* 20).toFixed(2);
+   } else if (over === "over 65 anni") {
+     // biglietto sconto 40% over 65
+     costobiglietto = costobiglietto - ((costobiglietto / 100)* 40).toFixed(2);
+   })
 });
