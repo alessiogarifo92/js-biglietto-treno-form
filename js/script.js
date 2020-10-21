6d1c3aab5c527ed76188feb97e1ae741c37bb7d0
alessiogarifo92 <alessiogarifo92@gmail.com>
2020-10-21 15:54:22 +0200

    impostazioni layout e inizio js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// km da percorrere
+var km = document.getElementById('km');
+// fascia età passegero
+var minorenne = document.getElementById('minorenne');
+var adulto = document.getElementById('adulto');
+var over65 = document.getElementById('over65');
+// costo biglietto intero
+var price = 0.21 * km;
+
+// costo biglietto minori di 18 anni
+
+// costo biglietto over 65 anni
